4cc774369ae5aa1dbe75ce61e4ee22c667a87420
Вадим <vadim@MacBook-Air-Vadim.local>
2023-09-25 16:11:03 +0400

    create news

--- src/components/AppCreateNews.vue
+++ src/components/AppCreateNews.vue
@@ -1,6 +1,7 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router';
 import axios from 'axios';
+export default {};
 
 tinymce.init({
   // Select the element(s) to add TinyMCE to using any valid CSS selector
@@ -12,20 +13,52 @@ tinymce.init({
   icons: 'thin',
   autosave_restore_when_empty: true,
 });
-
-export default {};
 </script>
 
 <template>
-  <div class="wrapperNews">
-    <input type="text" class="inputTitle" placeholder="заголовок новости" />
+  <div class="wrapperNews mt-1 mb-1">
+    <input type="text" name="title" class="titleInput" id="" placeholder="заголовок новости">
     <textarea id="myTextarea"> </textarea>
+    <button class="create-news">Создать</button>
   </div>
 </template>
 
 <style scoped>
-    .wrapperNews{
-        display: flex;
-        flex-direction: column;
-    }
+
+
+  .titleInput {
+    padding: 5px 7px;
+    background-color: transparent;
+    border: 1px solid #d5d5d5;
+    box-shadow: 0px 0 10px 0 #ffffff71;
+    border-radius: 15px;
+    width: 30%;
+  }
+
+  .titleInput::placeholder {
+    font-weight: 500;
+    color: black;
+  }
+  .create-news {
+    padding: 5px 15px;
+    background-color: transparent;
+    border: 1px solid #62A87C;
+    border-radius: 10px;
+    color: #62A87C;
+    font-weight: 600;
+  }
+
+  textarea {
+    width: 90%;
+    resize: none;
+    height: 500px;
+  }
+  .wrapperNews{
+    width: 100%;
+    gap: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
 </style>
